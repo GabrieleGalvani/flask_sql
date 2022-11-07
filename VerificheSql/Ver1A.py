66bd1255914bb43386ba6dfa110ed7e851eae4c0
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2022-11-07 08:45:15 +0000

    .

--- VerificheSql/Ver1A.py
+++ VerificheSql/Ver1A.py
@@ -14,9 +14,21 @@ import pymssql
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("home.html")
+    return render_template("home1A.html")
 
 
+@app.route("/ricerca", methods=["GET"])
+def ricerca():
+    nomestore = request.args['store']
+    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='dilecce.gabriele', password='xxx123##', database='dilecce.gabriele')
+    query = f"SELECT sf.first_name, sf.last_name FROM sales.stores AS st INNER JOIN sales.staffs AS sf ON as.store_id = st.store_id WHERE st.store_name  '{nomestore}'"
+    df= pd.read_sql
+    if df.values.tolist() == []:
+        return render_template("errore.html")
+    else:
+        return render_template("risultati1A.html", nomiColonne = df.columns.values, dati = list(df.values.tolist()))
+
+    
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
